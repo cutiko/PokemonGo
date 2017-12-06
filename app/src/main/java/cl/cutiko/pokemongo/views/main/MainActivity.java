5b9378169bf3cad39e84d5913de4441f94865071
cutiko <cutiko@gmail.com>
2017-12-06 14:03:46 -0300

    FIX: marking removing

--- app/src/main/java/cl/cutiko/pokemongo/views/main/MainActivity.java
+++ app/src/main/java/cl/cutiko/pokemongo/views/main/MainActivity.java
@@ -17,7 +17,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private static final int RC_GEO_PERMISSIONS = 456;
     private GoogleMap mGoogleMap;
+    private Marker mMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,11 +75,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 List<Location> locations = locationResult.getLocations();
                 if (locations.size() > 0) {
                     Location location = locations.get(0);
-                    mGoogleMap.clear();
+                    if (mMarker != null) {
+                        mMarker.remove();
+                    }
                     double latitude = location.getLatitude();
                     double longitude = location.getLongitude();
                     LatLng latLng = new LatLng(latitude, longitude);
-                    mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                    mMarker = mGoogleMap.addMarker(new MarkerOptions().position(latLng));
                     mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
                     new NearPokeStops(MainActivity.this).execute(latitude, longitude);
                 }
@@ -100,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void results(List<PokeStop> pokeStops) {
         for (PokeStop pokeStop : pokeStops) {
             LatLng latLng = new LatLng(pokeStop.getLatitude(), pokeStop.getLongitude());
-            mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+            mGoogleMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
         }
     }
 }
